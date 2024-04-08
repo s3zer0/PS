9ece32070a065b00b0b26642dbeefa387e03b487
s3zer0 <127709609+choeseyeong@users.noreply.github.com>
2024-04-08 15:05:36 +0900

    Update 6198.swift
    shorter Ver.

--- BOJ/6198.swift
+++ BOJ/6198.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 let n = Int(readLine()!)!
 var stack = [Int]()
 var answer = 0
@@ -7,22 +5,16 @@ var answer = 0
 for _ in 0..<n {
     let h = Int(readLine()!)!
     
-    if stack.isEmpty {
-        stack.append(h)
-    } else {
-        while !stack.isEmpty && stack.last! <= h {
-            stack.removeLast()
-            answer += stack.count
-        }
-        stack.append(h)
-    }
-}
-
-if stack.count > 1 {
-    for _ in 0..<stack.count-1 {
+    while !stack.isEmpty && stack.last! <= h {
         stack.removeLast()
         answer += stack.count
     }
+    
+    stack.append(h)
+}
+
+if stack.count != 1 {
+    answer += stack.count * (stack.count - 1) / 2
 }
 
 print(answer)
