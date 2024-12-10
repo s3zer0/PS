35195e38e3a4f2876cb9d6f65d3b989415d3dec2
s3zer0 <127709609+s3zer0@users.noreply.github.com>
2024-12-10 18:53:08 +0900

    Update 소수 찾기.swift

--- programmers/소수 찾기.swift
+++ programmers/소수 찾기.swift
@@ -4,57 +4,35 @@ func isPrime(_ number: Int) -> Bool {
     if number < 2 { return false }
     if number < 4 { return true }
     
-    for i in 2..<Int(sqrt(Double(number)) + 1) {
+    if number % 2 == 0 { return false }
+    
+    let limit = Int(sqrt(Double(number)))
+    for i in stride(from: 3, through: limit, by: 2) {
         if number % i == 0 { return false }
     }
     
     return true
 }
 
-func permutation(_ target: [String], _ targetNum: Int) -> [Int] {
-    var result: [[String]] = []
-    var check = [Bool](repeating: false, count: target.count)
-    
-    func permute(_ arr: [String]) {
-        if arr.count == targetNum {
-            result.append(arr)
-            return
-        }
-        for i in 0..<target.count {
-            if check[i] == true {
-                continue
-            } else {
-                check[i] = true
-                permute(arr + [target[i]])
-                check[i] = false
-            }
-        }
+func generatePermutations(_ numbers: [String], _ current: String, _ visited: inout [Bool], _ result: inout Set<Int>) {
+    if !current.isEmpty {
+        result.insert(Int(current)!)
     }
     
-    permute([])
-    
-    return result.map{ Int($0.joined())! }
+    for i in 0..<numbers.count {
+        if visited[i] { continue }
+        visited[i] = true
+        generatePermutations(numbers, current + numbers[i], &visited, &result)
+        visited[i] = false
+    }
 }
 
-func solution(_ numbers:String) -> Int {
-    let numberArray: [String] = Array(numbers).map{String($0)}
-    var primeCounts = 0
+func solution(_ numbers: String) -> Int {
+    let numberArray = Array(numbers).map { String($0) }
+    var visited = [Bool](repeating: false, count: numberArray.count)
+    var uniqueNumbers = Set<Int>()
     
-    var allCases: [Int] = []
+    generatePermutations(numberArray, "", &visited, &uniqueNumbers)
     
-    for i in 1...numberArray.count {
-        let permutations = Set(permutation(numberArray, i))
-        allCases.append(contentsOf: permutations)
-    }
-    
-    let setAllCases = Set(allCases)
-    
-    for number in setAllCases {
-        if isPrime(number) { primeCounts += 1 }
-    }
-    
-    return primeCounts
+    return uniqueNumbers.filter(isPrime).count
 }
-
-print(solution("17"))
-print(solution("011"))
